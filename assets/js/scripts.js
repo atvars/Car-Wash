3192f4cdef4035d5486933f42b932545d8210031
atvars <avelavs@gmail.com>
2020-12-31 00:54:49 +0000

    added scripts.js file and made my price info toggle as i press button

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+ $("#plan1Button").on("click",function() {
+        $("#info").toggle(1000);
+    });
+
+    $("#plan2Button").on("click",function() {
+        $("#info2").toggle(1000);
+    });
+
+    $("#plan3Button").on("click",function() {
+        $("#info3").toggle(1000);
+    });
